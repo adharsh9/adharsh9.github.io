a944ad483196e2508c6dc6fe24e3b82c9737a1f7
adharsh9 <adharshadhikari@Hotmail.com>
2020-01-25 20:55:11 -0600

    Add files via upload

--- photoinside.js
+++ photoinside.js
@@ -6,10 +6,10 @@
 
 var elements = document.getElementsByClassName("column");
 
-
+// Declare a loop variable
 var i;
 
-
+// Full-width images
 function one() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.flex = "100%";
